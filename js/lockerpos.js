680c7224b519df28290f9d35c6e823217dec3e9f
1F10220146 <s1f102201469@iniad.org>
2022-09-22 16:35:51 +0900

    列、行の表示を追加

--- js/lockerpos.js
+++ js/lockerpos.js
@@ -11,14 +11,19 @@ $(document).on("click", "#search-btn", () => {
                 xhr.setRequestHeader("Authorization", `Basic ${btoa(`${userName}:${userPass}`)}`)
             },
             success: (data) => {
-                const LOCKER_PATTERN = /^(\d{1})(\d{1})\d{2}\d{2}/;
+                const LOCKER_PATTERN = /^(\d{1})(\d{1})(\d{2})(\d{2})/;
                 const patterns = data.name.match(LOCKER_PATTERN);
                 const floor = patterns[1];
                 const street = patterns[2];
-    
+                const col = patterns[3];
+                const row = patterns[4];
+
+
                 $("#floor").text(floor);
                 $("#street").text(street);
-                
+                $("#col").text(col);
+                $("#row").text(row);
+
                 const insertImg = `<img src="img/${floor}.jpg">`;
 
                 $(".place").html(insertImg);
