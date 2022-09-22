a03b7bd47fd1661f22a09e46d241b40ef7e3537e
Hiroki Akahoshi <akaho1421@gmail.com>
2022-09-22 16:04:33 +0900

    画像挿入機能完成

--- js/lockerpos.js
+++ js/lockerpos.js
@@ -2,21 +2,30 @@ $(document).on("click", "#search-btn", () => {
     const userName = $("#user").val();
     const userPass = $("#pass").val();
 
-    $.ajax({
-        url: "https://api.iniad.org/api/v1/locker",
-        type: "GET",
-        dataType: "json",
-        beforeSend: (xhr) => {
-            xhr.setRequestHeader("Authorization", `Basic ${btoa(`${userName}:${userPass}`)}`)
-        },
-        success: (data) => {
-            const LOCKER_PATTERN = /^(\d{1})(\d{1})\d{2}\d{2}/;
-            const patterns = data.name.match(LOCKER_PATTERN);
-            const floor = patterns[1];
-            const street = patterns[2];
+    setTimeout(() => {
+        $.ajax({
+            url: "https://api.iniad.org/api/v1/locker",
+            type: "GET",
+            dataType: "json",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", `Basic ${btoa(`${userName}:${userPass}`)}`)
+            },
+            success: (data) => {
+                const LOCKER_PATTERN = /^(\d{1})(\d{1})\d{2}\d{2}/;
+                const patterns = data.name.match(LOCKER_PATTERN);
+                const floor = patterns[1];
+                const street = patterns[2];
+    
+                $("#floor").text(floor);
+                $("#street").text(street);
+                
+                const insertImg = `<img src="img/${floor}.jpg">`;
 
-            $("#floor").text(floor);
-            $("#street").text(street);
-        }
-    })
+                $(".place").html(insertImg);
+            },
+            error: (data) => {
+                console.log(data);
+            }
+        })
+    }, 500);
 });
